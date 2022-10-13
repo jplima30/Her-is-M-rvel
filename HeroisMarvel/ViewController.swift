01bc77f2249fe25ac50652235613af835159f43b
jplima <jplimadev@gmail.com>
2022-10-13 13:26:33 -0300

    Sync

---
+++ HeroisMarvel/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+}
+
