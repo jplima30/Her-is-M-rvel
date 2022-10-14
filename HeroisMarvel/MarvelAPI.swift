815768e13e016b72ef3a219baacddfa79d7d64e6
jplima <jplimadev@gmail.com>
2022-10-14 15:25:19 -0300

    APIconfig

---
+++ HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftHash
+import Alamofire
+
+class MarvelAPI {
+    
+    static private let basePath = "http://gateway.marvel.com/v1/public/characters"
+    static private let privateKey = "7cafb5038f0a2e88e8e13bdaec0a04f547a4ef1d"
+    static private let publicKey = "dc3a63ea70da359d895a29f35e8a98e5"
+    static private let limit = 50
+    
+    class func loadHeros(name: String?, page: Int = 0, onComplete: @escaping (MarvelInfo?) -> Void) {
+        let offset = page * limit
+        let startsWith: String
+        if let name = name, !name.isEmpty {
+            startsWith = "nameStartsWith=\(name.replacingOccurrences(of: " ", with: ""))&"
+        } else {
+            startsWith = ""
+        }
+        
+        let url = basePath + "offset=\(offset)&limit=\(limit)&" + startsWith + getcredentials()
+        print(url)
+        AF.request(url).responseJSON { (response) in
+            guard let data = response.data,
+                  let marvelInfo = try? JSONDecoder().decode(MarvelInfo.self, from: data), marvelInfo.code == 200 else {
+                onComplete(nil)
+                return
+            }
+            onComplete(marvelInfo)
+        }
+        
+    }
+    
+    private class func getcredentials() -> String {
+        let timeStemp = String(Date().timeIntervalSince1970)
+        let hash = MD5(timeStemp+privateKey+publicKey).lowercased()
+        return "ts=\(timeStemp)1&apikey=\(publicKey)&hash=\(hash)"
+    }
+}
